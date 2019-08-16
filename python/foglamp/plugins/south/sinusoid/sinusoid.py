70f704b3841af5e70de6cd85f0387428ea0f731e
Singhal-Vaibhav <vaibhav@scaledb.com>
2019-08-16 13:25:42 -0400

    plugin info version bumped

--- python/foglamp/plugins/south/sinusoid/sinusoid.py
+++ python/foglamp/plugins/south/sinusoid/sinusoid.py
@@ -119,7 +119,7 @@ def plugin_info():
     """
     return {
         'name': 'Sinusoid Poll plugin',
-        'version': '1.5.0',
+        'version': '1.7.0',
         'mode': 'poll',
         'type': 'south',
         'interface': '1.0',
